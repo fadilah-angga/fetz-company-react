09041cde1ff9406abfe50882ab7409ed3871076f
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2025-02-06 14:09:44 +0700

    update ui services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,22 @@
 import { motion } from "framer-motion";
 
 export default function Services() {
-  const services = [
-    {
-      title: "Web Development",
-      description:
-        "Membangun website yang menarik, mudah digunakan, dan siap membantu bisnismu berkembang!",
-      image: "/img/vector-2.png",
-    },
-    {
-      title: "UI/UX Design",
-      description:
-        "Mendesain antarmuka pengguna yang nyaman dan menarik agar pengguna betah dan mudah memahami aplikasi atau websitemu!",
-      image: "/img/vector-3.png",
-    },
-  ];
-
   return (
-    <section id="services" className="py-16 relative">
+    <section id="services" className="py-20 relative">
+      <div
+        className="absolute inset-0 bg-no-repeat bg-center"
+        style={{
+          backgroundImage: "url('/img/services.png')",
+          backgroundSize: "100% auto",
+        }}
+      ></div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-60"></div>
       <div className="container mx-auto text-center relative">
         {/* Judul */}
         <motion.h2
-          className="font-montserrat text-customBlack font-semibold text-3xl mb-12 relative z-10"
+          className="font-montserrat text-customWhite font-semibold text-3xl relative z-10"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
@@ -30,33 +25,102 @@ export default function Services() {
           Layanan <span className="gradient-text">Kami</span>
         </motion.h2>
 
-        {/* Gambar Background di Bawah Kiri */}
-        <img
-          src="/img/circle-bg.png"
-          alt="bg"
-          className="absolute bottom-0 left-0 transform -translate-x-2/4 translate-y-1/3 z-0 w-[650px] opacity-50"
-        />
+        {/* Card Section */}
+        <div className="relative z-10 mt-24 mb-5 w-full font-jakarta text-center flex justify-center gap-8">
+          {/* Card 1 */}
+          <div className="w-80 bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg hover:scale-105 transition-all duration-300">
+            <div className=" flex items-center justify-center">
+              <div className="rounded-full my-5 w-36 h-36 bg-customWhite flex items-center justify-center">
+                <img src="/img/vector-2.png" alt="modern" width={120} />
+              </div>
+            </div>
+            <h2 className="text-xl font-bold text-white">
+              Jasa Pembuatan <span className="gradient-text">Website</span>
+            </h2>
+            <p className="text-gray-300 my-5">
+            Membangun website yang menarik, responsif, dan mudah digunakan untuk mendukung pertumbuhan bisnismu dengan teknologi terkini!            </p>
+            <a href="#services" className="flex justify-center my-8">
+              <button className="cta group flex items-center px-3 py-2 text-2xl text-white bg-customGreen transition-all duration-1000 transform skew-x-[-15deg] shadow-[6px_6px_0_black] border-none cursor-pointer focus:outline-none">
+                <span className="span font-montserrat text-sm transform skew-x-[15deg]">
+                  SELENGKAPNYA
+                </span>
+                <span className="second relative ml-3 translate-y-[-12%] w-5 transition-all duration-500 group-hover:mr-[20px]">
+                  <svg
+                    width="40px"
+                    height="15px"
+                    viewBox="0 0 66 43"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="arrow" fill="none">
+                      <path
+                        className="one transition-all duration-400 transform translate-x-[-60%]"
+                        d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                      <path
+                        className="two transition-all duration-500 transform translate-x-[-30%]"
+                        d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                      <path
+                        className="three transition-all duration-200"
+                        d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                    </g>
+                  </svg>
+                </span>
+              </button>
+            </a>
+          </div>
 
-        {/* Card Layanan */}
-        <div className="flex flex-wrap justify-center gap-14 relative z-10">
-          {services.map((service, index) => (
-            <motion.div
-              key={index}
-              className="bg-white cursor-pointer shadow-lg rounded-2xl p-6 shadow-slate-200 hover:shadow-slate-300 hover:shadow-2xl transition-shadow flex flex-col items-center text-center w-[300px]"
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 2 }}
-            >
-              <img src={service.image} alt={service.title} className="w-52 mb-4" />
-              <h3 className="text-xl font-montserrat font-semibold gradient-text mb-2">
-                {service.title}
-              </h3>
-              <p className="text-customBlack font-montserrat text-sm">
-                {service.description}
-              </p>
-            </motion.div>
-          ))}
+          {/* Card 2 */}
+          <div className="w-80 bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg hover:scale-105 transition-all duration-300">
+            <div className=" flex items-center justify-center">
+              <div className="rounded-full my-5 w-36 h-36 bg-customWhite flex items-center justify-center">
+                <img src="/img/vector-3.png" alt="modern" width={120} />
+              </div>
+            </div>
+            <h2 className="text-xl font-bold text-white">
+              Jasa Pembuatan <span className="gradient-text">UI / UX</span>
+            </h2>
+            <p className="text-gray-300 my-5">
+              Mendesain antarmuka pengguna yang nyaman dan menarik agar pengguna betah dan mudah memahami aplikasi atau websitemu!
+            </p>
+            <a href="#services" className="flex justify-center my-8">
+              <button className="cta group flex items-center px-3 py-2 text-2xl text-white bg-customGreen transition-all duration-1000 transform skew-x-[-15deg] shadow-[6px_6px_0_black] border-none cursor-pointer focus:outline-none">
+                <span className="span font-montserrat text-sm transform skew-x-[15deg]">
+                  SELENGKAPNYA
+                </span>
+                <span className="second relative ml-3 translate-y-[-12%] w-5 transition-all duration-500 group-hover:mr-[20px]">
+                  <svg
+                    width="40px"
+                    height="15px"
+                    viewBox="0 0 66 43"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="arrow" fill="none">
+                      <path
+                        className="one transition-all duration-400 transform translate-x-[-60%]"
+                        d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                      <path
+                        className="two transition-all duration-500 transform translate-x-[-30%]"
+                        d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                      <path
+                        className="three transition-all duration-200"
+                        d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                        fill="#FFFFFF"
+                      />
+                    </g>
+                  </svg>
+                </span>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </section>
